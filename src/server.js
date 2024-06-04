4974ae36955ce66eaa3d3214abbc936ed7184c91
With0utN3me <sinitsin678@gmail.com>
2024-06-04 14:07:38 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ export const setupServer = () => {
         });
     });
 
-    app.get('/contacts:contactId', async (req, res) => {
+    app.get('/contacts/:contactId', async (req, res) => {
         const { contactId } = req.params;
         const contact = await getContactById(contactId);
 
