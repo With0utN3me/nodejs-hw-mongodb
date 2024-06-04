8dc683180ce1612479f6e92cf00e2098387db0d4
With0utN3me <sinitsin678@gmail.com>
2024-06-04 13:49:01 +0200

    update env files

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,19 +1,21 @@
-
 import mongoose from 'mongoose';
-
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const initMongoConnection = async () => {
     try {
-        const user = process.env['MONGODB_USER'];
-        const pwd = process.env['MONGODB_PASSWORD'];
-        const url = process.env['MONGODB_URL'];
-        const db = process.env['MONGODB_DB'];
+        const user = process.env.MONGODB_USER;
+        const pwd = process.env.MONGODB_PASSWORD;
+        const url = process.env.MONGODB_URL;
+        const db = process.env.MONGODB_DB;
+
+        if (!user || !pwd || !url || !db) {
+            throw new Error('Missing environment variables for MongoDB connection');
+        }
 
         await mongoose.connect(
-            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`
         );
         console.log('Mongo connection successfully established!');
     } catch (e) {
