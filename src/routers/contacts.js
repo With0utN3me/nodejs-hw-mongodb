1a7d006573fd56a4177c6bf0544e92ff41e6ddf0
With0utN3me <sinitsin678@gmail.com>
2024-06-24 21:54:26 +0200

    some part of hw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,13 +5,13 @@ import { createContactSchema, updateContactSchema } from "../validation/contacts
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
-router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/contacts/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
+router.patch('/:contactId', validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
 export default router;
