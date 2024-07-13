e57331b542943b43fd03a3d05ab9da6488dfca9b
With0utN3me <sinitsin678@gmail.com>
2024-07-13 22:33:17 +0200

    some part completed

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -25,6 +26,9 @@ export const setupServer = () => {
     );
 
     app.use(cookieParser());
+    
+    app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
     
     app.use('/uploads', express.static(UPLOAD_DIR));
     
