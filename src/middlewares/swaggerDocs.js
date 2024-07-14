2d2875424be294798f97a25a26b4a66520d4ac36
With0utN3me <sinitsin678@gmail.com>
2024-07-14 12:36:16 +0200

    update

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -8,9 +8,8 @@ export const swaggerDocs = () => {
     try {
         const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
         return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-    } catch (err) {
-        return (req, res, next) =>
-        next(createHttpError(500, "Can't load swagger docs")),
-        console.log(err);
+    }  catch (err) {
+        console.error(`Error loading Swagger docs: ${err.message}`);
+        return (req, res, next) => next(createHttpError(500, "Can't load swagger docs"));
     }
 };
