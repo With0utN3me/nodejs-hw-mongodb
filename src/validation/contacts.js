3bedec176a9f7688fbfc2b9ecd179ef98782b3fe
With0utN3me <sinitsin678@gmail.com>
2024-07-15 10:57:41 +0200

    allow to post contact with empty params

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,10 +3,10 @@ export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
     phoneNumber: Joi.string().min(3).max(20).required(),
     email: Joi.string().min(3).max(20).email(),
-    isFavourite: Joi.boolean(),
-    contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
-    photo: Joi.string(),
-}).unknown(true);
+    isFavourite: Joi.boolean().optional(),
+    contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal').allow('').optional(),
+    photo: Joi.string().allow('').optional(),
+}).options({ stripUnknown: true });
 
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).allow('').optional(),
